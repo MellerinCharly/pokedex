1d302277e6c264aaccdb9b0e5b6829a611c73f9c
MellerinCharly <mellerincharly.pro@gmail.com>
2024-10-30 09:28:02 +0100

    Change pokemoncard and app.tsx encore...

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
 
+function App() {
+  return (
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
+  );
+}
+
 const pokemonList = [
   {
-    name: "Bulbasaur",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
     name: "mew",
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} /> 
-    </div>
-  );
-}
-
 
 export default App;
